a7994d37ea9b0884af7ee15cbb52021beed7ad48
Dineshchandra47 <dineshchandraindia5@gmail.com.com>
2023-01-07 18:19:59 +0530

    Updated Frontend

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./components/signinAndRegister/landing";
+import Todo from "./components/todo/todo";
+
+function App() {
+  const [user, setUser] = useState({});
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing setUser={setUser} />} />
+        <Route path="/todo" element={<Todo user={user} setUser={setUser} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/signinAndRegister/landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Signin from "./signin";
+import "./landing.css";
+
+function Landing({ setUser }) {
+  const [content, setContent] = useState();
+  useEffect(() => {
+    setContent(<Signin setUser={setUser} setContent={setContent} />);
+    // eslint-disable-next-line 
+  }, []);
+
+  return <div className="landing">{content}</div>;
+}
+
+export default Landing;
